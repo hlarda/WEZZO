f06c51b9cc4715cecc84433fc7970816c214f50a
hlarda <halareda63@gmail.com>
2024-10-23 07:11:48 +0300

    Increase the request's timeout duration

--- app/src/main/java/com/example/wezzo/model/remote/NetworkService.kt
+++ app/src/main/java/com/example/wezzo/model/remote/NetworkService.kt
@@ -3,13 +3,22 @@ package com.example.wezzo.model.remote
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 object NetworkService {
     private const val BASE_URL = "https://api.openweathermap.org/"
 
+    private val okHttpClient= OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+
     val retrofitService: NetworkInterface by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(NetworkInterface::class.java)
