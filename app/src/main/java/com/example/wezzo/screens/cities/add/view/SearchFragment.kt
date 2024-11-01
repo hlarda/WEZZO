647665832c6edc1a4d59d08d44d877b13e08764b
hlarda <halareda63@gmail.com>
2024-11-01 14:06:49 +0200

    current location button to main

--- app/src/main/java/com/example/wezzo/screens/cities/add/view/SearchFragment.kt
+++ app/src/main/java/com/example/wezzo/screens/cities/add/view/SearchFragment.kt
@@ -1,13 +1,17 @@
 package com.example.wezzo.screens.cities.add.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.wezzo.R
 import com.example.wezzo.databinding.FragmentAddCityBinding
+import com.google.android.gms.location.LocationServices
 
 class AddCityFragment : Fragment() {
 
@@ -26,7 +30,30 @@ class AddCityFragment : Fragment() {
         }
 
         binding.currentLocationButton.setOnClickListener {
-            findNavController().navigate(R.id.action_SearchFragment_to_FirstFragment)
+            // Fetch the current location
+            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    1
+                )
+            } else {
+                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                    val bundle = Bundle()
+                    bundle.putDouble("latitude", location.latitude)
+                    bundle.putDouble("longitude", location.longitude)
+                    findNavController().navigate(R.id.action_SearchFragment_to_FirstFragment, bundle)
+                }
+            }
+
         }
 
         return view
